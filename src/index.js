6d320fd68bfefbfe90aeab3990d90299219b0937
kvtvs <kata.sara-aho@metropolia.fi>
2022-01-30 22:39:23 +0200

    week3-task3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ FazerData;
 let language = 'fi';
 // let currentMenu = SodexoData.coursesFI;
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 console.log(SodexoData);
 console.log(FazerData);
 /**
